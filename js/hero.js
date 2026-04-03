14b21a655c5661dfcc6295594174e2962b1ef9c2
khanhlam <khanhlam.photo@gmail.com>
2026-04-03 08:44:41 +0700

    Delete js directory

--- js/hero.js
+++
@@ -1,7 +0,0 @@
-async function loadSection(id, file) {
-  const res = await fetch(file);
-  const html = await res.text();
-  document.getElementById(id).innerHTML = html;
-}
-
-loadSection('hero-section', '/sections/hero.html');
